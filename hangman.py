ff100d0bea0f774ccfd8f05d67c0a29c58806fe5
Connor Meads <meads.connoredward@gmail.com>
2020-05-09 14:22:47 -0600

    First commit

---
+++ hangman.py
@@ -0,0 +1,3 @@
+if __name__ == "__main__":
+    #Introduction to the game
+    print("Hello & welcome to my first real python program.\nI will be attempting to make a hangman game in the console.\nFurther reading can be found on the README.md.\nEnjoy :)")
