8b136eb38fb5de856f52037dc4ccabe170cdf138
Connor Meads <meads.connoredward@gmail.com>
2020-05-13 21:50:52 -0600

    Added a default phrases .txt file.  Also added functionality to input your own custom phrase.  Also added functionality to import your own custom .txt file.  Added loops to catch mistakes and to return to the main menu.

--- hangman.py
+++ hangman.py
@@ -1,25 +1,60 @@
-if __name__ == "__main__":
-    #Introduction to the game
-    print("Hello & welcome to my first real python program.\nI will be attempting to make a hangman game in the console.\nFurther reading can be found on the README.md.\nEnjoy :)")
+import os.path
+import random
 
-    #Gather user settings
-    while True:
-        difficultySetting = input("\nSELECT A DIFFICULTY:\nEasy - 1\nMedium - 2\nHard - 3\nCustom - 4\n:")
+def readFile(l_fileName):
+    if os.path.isfile(l_fileName):
         try:
-            difficultySetting = int(difficultySetting)
-            if(difficultySetting < 1 or difficultySetting > 4):
-                print("**Error:  You selected a value outside of 1-4.  Please enter in a correct value")
-            else:
-                break
-        except ValueError:
-            print("**Error: You did not enter in a number between 1-4.  Please select a correct difficulty...")
-    
-    #Depending on the input, gather the word[s] the user will be guessing
-    if(difficultySetting == 1):
-        print("You selected 1")
-    elif(difficultySetting == 2):
-        print("you selected 2")
-    elif(difficultySetting == 3):
-        print("you selected 3")
+            with open(l_fileName, "r", encoding="utf-16") as idiomFile:
+                #An array of strings containing the contents of 'idioms.txt'
+                return idiomFile.readlines()   
+        except FileNotFoundError:
+            print("There was an error in reading the file")
     else:
-        print("you selected the custom option1!")
+        return []
+
+if __name__ == "__main__":
+    #Local Variables
+    phrases = []
+    returnToMainMenu = True
+
+    #Introduction to the game
+    print("This hangman game will increase your use of idioms.  Enjoy\n*Made by Connor Meads*\n")
+    
+    while(returnToMainMenu):
+        choiceInput = input("1 - Use my list of idioms\n2 - Type in your own custom phrase\n3 - Type in the name of the .txt file for your own custom phrases\n: ")
+
+        if(choiceInput == '1'): #If user wants to use my file, send 'idioms.txt as the choiceInput
+            phrases = readFile("idioms.txt")
+        elif(choiceInput == '2'):
+            while(True):
+                correctPhrase = True
+                customPhrase = input("Enter in your custom phrase or press '1' to return: ")
+                if(customPhrase == '1'):
+                    break
+                for element in customPhrase: #Figure out if the custom phrase contains correct characters
+                    if ((ord(element) >= 0 and ord(element) <= 31) or (ord(element) >= 33 and ord(element) <= 64) or (ord(element) >= 91 and ord(element) <= 96) or (ord(element) >= 123)):
+                        correctPhrase = False
+                        print("The character {0} is invalid.".format(element))
+                if(correctPhrase):
+                    phrases.append(customPhrase)
+                    break
+                else:
+                    print("Please only enter letters and spaces.  Please try again...")
+
+        elif(choiceInput == '3'): #If user wants to use their own file, test it for a file extension
+            fileName = input("Enter in the name of the .txt file you would like to use or press 1 to return: ")
+            if(fileName == '1'):
+                pass
+            elif(fileName[-4:] == '.txt'): #Tests to see if the user inputted file has a '.txt. extension on it
+                phrases = readFile(fileName)
+            else: #If the user inpufileNameoesn't have a '.txt' on it, smack it on
+                phrases = readFile(fileName + '.txt')
+
+        if phrases != []: #This is correct output
+            break
+        else:
+            print("\n...Returning to main menu...\n")
+    
+    #Now pick a random phrase from the choices
+    currentPhrase = random.choice(phrases)
+    print(currentPhrase)
